957443a581aee68eb38d16884c279b634515aee7
AdaptiveX <190427897+Adaptvx@users.noreply.github.com>
2026-07-18 22:32:28 -0700

    1.5.4

--- Locales/ruRU.lua
+++ Locales/ruRU.lua
@@ -12,6 +12,10 @@ L["CANCEL"] = "Cancel"
 L["WAY_PASTE_PROMPT"] = "Enter /way commands"
 
 -- Font
+L["TEXT_ALIGNMENT"] = "Text Alignment"
+L["LEADING"] = "Leading"
+L["JUSTIFIED"] = "Justified"
+L["TRAILING"] = "Trailing"
 L["FONT_FLAGS"] = "Флаги шрифта"
 L["NONE"] = "Нет"
 L["OUTLINE"] = "Контур"
